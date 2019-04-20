f5e899c20c4ae624034c987ca2720649d259aebb
Alex0ne <alex0ne@localhost.localdomain>
2019-04-20 11:54:24 +0300

    App Skeleton created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,28 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { ComponentsModule } from './components/components.module';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { AuthGuard } from './auth/auth.guard';
 
-const routes: Routes = [];
+
+
+const routes : Routes = [
+    {path: '', pathMatch: 'full', component: WelcomePageComponent},
+    { path: 'authentication', children: [
+        {path: 'signin', component: SigninComponent},
+        {path: 'signup' ,component: SignupComponent}
+    ] },
+    { path: 'components', 
+     loadChildren: () => ComponentsModule,
+     canActivate:[AuthGuard] 
+  }
+
+]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [ RouterModule.forRoot(routes) ],
+    exports: [ RouterModule ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+// import { ToastrModule } from 'ngx-toastr'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+// import { TokenInterceptor } from './interceptors/token.interceptor';
+import { WelcomePageComponent } from './components/welcome-page/welcome-page.component';
+import { ComponentsModule } from './components/components.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    WelcomePageComponent,
+ 
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AuthModule,  
+    // ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ComponentsModule
   ],
-  providers: [],
+  providers: [
+    // {
+    //   provide: HTTP_INTERCEPTORS,
+    //   useClass: TokenInterceptor,
+    //   multi: true
+    // }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
